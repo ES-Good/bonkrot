7866de954aca857f20b43e412ad58e111bf632f8
Eduard Gorshkov <britvahp@gmail.com>
2021-02-18 17:00:07 +0400

    section comments

---
+++ js/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+  $('.variable-width').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
+
+});
