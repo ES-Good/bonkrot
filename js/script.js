404b620a270bcc74a29a43a0e5b72cbe990123a0
Eduard Gorshkov <britvahp@gmail.com>
2021-02-19 15:36:27 +0400

    section internet

--- js/script.js
+++ js/script.js
@@ -9,4 +9,18 @@ window.addEventListener('DOMContentLoaded', () => {
     variableWidth: true
   });
 
+  const more = document.querySelector('.internet__more'),
+        cards = document.querySelectorAll('.internet__card--none');
+
+  more.addEventListener('click', () => {
+    cards.forEach(card => {
+      card.classList.toggle('internet__card--none');
+      if (!card.classList.contains('internet__card--none')) {
+        more.textContent = 'Скрыть';
+      } else {
+        more.textContent = 'Показать ещё...';
+      }
+    });
+  });
+
 });
